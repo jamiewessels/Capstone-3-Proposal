610e00576e17e4200cb79337f4ca5992ef34d162
Jamie Wessels <jamiewessel11@gmail.com>
2020-09-14 15:56:43 -0400

    images

---
+++ directory.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+ds = tf.keras.preprocessing.image_dataset_from_directory(
+    'data/train',
+    labels="inferred",
+    label_mode="int",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=32,
+    image_size=(256, 256),
+    shuffle=True,
+    seed=None,
+    validation_split=None,
+    subset=None,
+    interpolation="bilinear",
+    follow_links=False,
+)
--- transfer_learning_5_chords.py
+++ transfer_learning_5_chords.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.layers import Activation, Convolution2D, Dense, Dropout, Flatten, MaxPooling2D, GlobalAveragePooling2D
-from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.models import Sequential, Model, clone_model
 from tensorflow.keras.applications.xception import preprocess_input
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.losses import CategoricalCrossentropy
@@ -24,7 +24,7 @@ valid_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
 
-def create_transfer_model(base_model, n_categories=3):
+def create_transfer_model(base_model, n_categories=5):
         
         # add new head
         avg = GlobalAveragePooling2D()(base_model.output)
@@ -55,13 +55,12 @@ def score_model(model, test_generator, num_test):
         yhat = np.argmax(probas, axis = 1)
         results_dict['recall'] = recall_score(ytrue, yhat, average = 'macro')
         results_dict['precision'] = precision_score(ytrue, yhat, average = 'macro')
-        results_dict['roc_auc'] = roc_auc_score(ytrue, yhat)
         return results_dict 
         
 
 if __name__ == '__main__':
 
-        num_train = len([os.path.join(path, name) for path, subdirs, files in os.walk('data/train/') for name in files])+1
+        num_train = len([os.path.join(path, name) for path, subdirs, files in os.walk('data/train/augmented_2') for name in files])+1
         num_valid = len([os.path.join(path, name) for path, subdirs, files in os.walk('data/val') for name in files])+1
         num_test = len([os.path.join(path, name) for path, subdirs, files in os.walk('data/test') for name in files])+1
 
@@ -73,7 +72,7 @@ if __name__ == '__main__':
 
         # make flow objects
         train_generator = train_datagen.flow_from_directory(
-                'data/train/',
+                'data/train/augmented_2',
                 target_size=target_size,
                 batch_size=batch_size,
                 class_mode='categorical')
@@ -95,59 +94,69 @@ if __name__ == '__main__':
         run_tboard_dir = get_tboard_logdir()
         tensorboard = TensorBoard(log_dir=run_tboard_dir, histogram_freq=2, batch_size=batch_size, write_graph=True, write_grads=True, write_images=True)
         early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-        model_cp = ModelCheckpoint(monitor='val_loss', save_best_only=True, filepath='./CovNet_logs/Checkpoint1.hdf5')
-
-        #Create transfer model
         
-        base_model = Xception(weights='imagenet',
-                                include_top=False,
-                                input_shape=(299,299,3))
-        '''
-        transfer_model = create_transfer_model(base_model, n_categories=3)
+        
+        #Create transfer model based on weights from 3 classes (checkpoint6)
+        ''' 
+        weights = load_model('./CovNet_logs/Checkpoint6.hdf5').get_weights()[:-2]
+        base_model = Xception(weights= 'imagenet',
+                        include_top=False,
+                        input_shape=(299,299,3))
 
+        base_model.set_weights(weights)
+        
+        transfer_model = create_transfer_model(base_model, n_categories=5)
+
+        #First round, just change weights for the output layer
         _ = change_trainable_layers(transfer_model, 132)
-
         print_model_properties(transfer_model)
-        
 
-        #Compile and fit model
+        #Compile and fit first model - just outer layer tuning
         optimizer = SGD(lr=0.2, momentum = 0.9, decay = 0.01)
-        transfer_model.compile(optimizer=optimizer, loss=['categorical_crossentropy'], metrics=['accuracy'])
+        model_cp = ModelCheckpoint(monitor='val_loss', save_best_only=True, filepath='./CovNet_logs/Checkpoint1B.hdf5')
+        transfer_model.compile(optimizer=optimizer, loss=['categorical_crossentropy'], metrics=['accuracy', 'AUC'])
 
         history = transfer_model.fit(x=train_generator, 
                         validation_data=validation_generator,
-                        epochs=5, 
+                        epochs=50, 
                         steps_per_epoch=steps_per_epoch, 
                         validation_steps=validation_steps,
-                        callbacks=[tensorboard, model_cp])
+                        callbacks=[tensorboard, early_stopping, model_cp])
+        '''
         
-        model_cp2 = ModelCheckpoint(monitor='val_loss', save_best_only=True, filepath='./CovNet_logs/Checkpoint2.hdf5')
 
-        transfer_model_2 = load_model('./CovNet_logs/Checkpoint1.hdf5')
+        
+        transfer_model_2 = load_model('./CovNet_logs/Checkpoint1B.hdf5')
 
         _ = change_trainable_layers(transfer_model_2, 126) 
 
-        optimizer = SGD(lr=0.01, momentum = 0.9, decay = 0.001)
-        transfer_model.compile(optimizer=optimizer, loss=['categorical_crossentropy'], metrics=['accuracy'])
+        print_model_properties(transfer_model_2)
 
-        history = transfer_model.fit(x=train_generator, 
+        optimizer = SGD(lr=0.01, momentum = 0.9, decay = 0.001)
+        transfer_model_2.compile(optimizer=optimizer, loss=['categorical_crossentropy'], metrics=['accuracy', 'AUC'])
+        model_cp2 = ModelCheckpoint(monitor='val_loss', save_best_only=True, filepath='./CovNet_logs/Checkpoint2B.hdf5')
+
+        history = transfer_model_2.fit(x=train_generator, 
                         validation_data=validation_generator,
                         epochs=500, 
                         steps_per_epoch=steps_per_epoch, 
                         validation_steps=validation_steps,
                         callbacks=[model_cp2, early_stopping, tensorboard])
-        '''
-        '''
-        transfer_model_3 = load_model('./CovNet_logs/Checkpoint2.hdf5')
 
-        model_cp3 = ModelCheckpoint(monitor='val_loss', save_best_only=True, filepath='./CovNet_logs/Checkpoint3.hdf5')
+
+
+        '''
+        
+        transfer_model_3 = load_model('./CovNet_logs/Checkpoint8.hdf5')
+
+        model_cp3 = ModelCheckpoint(monitor='val_loss', save_best_only=True, filepath='./CovNet_logs/Checkpoint9.hdf5')
 
         _ = change_trainable_layers(transfer_model_3, 116) 
 
         print_model_properties(transfer_model_3)
 
         optimizer = SGD(lr=0.01, momentum = 0.9, decay = 0.001)
-        transfer_model_3.compile(optimizer=optimizer, loss=['categorical_crossentropy'], metrics=['accuracy'])
+        transfer_model_3.compile(optimizer=optimizer, loss=['categorical_crossentropy'], metrics=['accuracy', 'AUC'])
 
         history = transfer_model_3.fit(x=train_generator, 
                         validation_data=validation_generator,
@@ -155,12 +164,13 @@ if __name__ == '__main__':
                         steps_per_epoch=steps_per_epoch, 
                         validation_steps=validation_steps,
                         callbacks=[model_cp3, early_stopping, tensorboard])
-        '''
-        '''
+        
+        
+        
 
-        transfer_model_4 = load_model('./CovNet_logs/Checkpoint3.hdf5')
+        transfer_model_4 = load_model('./CovNet_logs/Checkpoint9.hdf5')
 
-        model_cp5 = ModelCheckpoint(monitor='val_loss', save_best_only=True, filepath='./CovNet_logs/Checkpoint5.hdf5')
+        model_cp4 = ModelCheckpoint(monitor='val_loss', save_best_only=True, filepath='./CovNet_logs/Checkpoint10.hdf5')
 
         _ = change_trainable_layers(transfer_model_4, 106) 
 
@@ -174,32 +184,33 @@ if __name__ == '__main__':
                         epochs=500, 
                         steps_per_epoch=steps_per_epoch, 
                         validation_steps=validation_steps,
-                        callbacks=[model_cp5, early_stopping, tensorboard])
+                        callbacks=[model_cp4, early_stopping, tensorboard])
 
-        '''
-        transfer_model_6 = load_model('./CovNet_logs/Checkpoint5.hdf5')
+        
 
-        model_cp6 = ModelCheckpoint(monitor='val_loss', save_best_only=True, filepath='./CovNet_logs/Checkpoint6.hdf5')
+        
+        transfer_model_5 = load_model('./CovNet_logs/Checkpoint10.hdf5')
+
+        model_cp5 = ModelCheckpoint(monitor='val_loss', save_best_only=True, filepath='./CovNet_logs/Checkpoint11.hdf5')
 
         _ = change_trainable_layers(transfer_model_6, 96) 
 
-        print_model_properties(transfer_model_6)
+        print_model_properties(transfer_model_5)
 
         optimizer = Adam(lr=0.001, beta_1 = 0.9, beta_2 = 0.999)
-        transfer_model_6.compile(optimizer=optimizer, loss=['categorical_crossentropy'], metrics=['accuracy', 'AUC'])
+        transfer_model_5.compile(optimizer=optimizer, loss=['categorical_crossentropy'], metrics=['accuracy', 'AUC'])
 
-        history = transfer_model_6.fit(x=train_generator, 
+        history = transfer_model_5.fit(x=train_generator, 
                         validation_data=validation_generator,
                         epochs=500, 
                         steps_per_epoch=steps_per_epoch, 
                         validation_steps=validation_steps,
                         callbacks=[model_cp6, early_stopping, tensorboard])
 
+        '''
 
-
-        best_model = load_model('./CovNet_logs/Checkpoint6.hdf5')
+        best_model = load_model('./CovNet_logs/Checkpoint2B.hdf5')
 
         metrics = score_model(best_model, test_generator, num_test)
 
-        for k, v in metrics.items():
-                print(f'{k} : {v}')
+        print(metrics)
