37f2203b7755b9af080040d91814150d490cb420
Jamie Wessels <jamiewessel11@gmail.com>
2020-09-15 16:02:51 -0400

    classify on video

---
+++ src/camera.py
@@ -0,0 +1,174 @@
+import cv2
+from predictions import ChordIdentifier
+import numpy as np
+from tensorflow.keras.models import load_model
+
+
+
+model = load_model("CovNet_logs/Checkpoint7.hdf5")
+font = cv2.FONT_HERSHEY_SIMPLEX
+org = (50, 50)
+fontScale = 1
+color = (1, 0, 0) 
+thickness = 2
+classes = np.array(['C', 'F', 'G'])
+
+cap = cv2.VideoCapture('src/quickvid.mov')
+frameRate = cap.get(5) #frame rate
+
+cap.set(cv2.CAP_PROP_FPS, 5)
+fps = int(cap.get(5))
+# print("fps:", fps)
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
+    gray = cv2.rotate(gray, cv2.cv2.ROTATE_90_CLOCKWISE)
+
+    roi = cv2.resize(gray, (299,299))/255
+    # roi = cv2.rotate(roi, cv2.cv2.ROTATE_90_CLOCKWISE)
+    # print(roi[1])
+    # new_img = np.delete(roi, 3 , axis = 2).reshape(-1, 299, 299, 3)
+
+    pred = classes[np.argmax(model.predict(np.delete(roi, 3 , axis = 2).reshape(-1, 299, 299, 3)), axis = 1)][0]
+    # print(pred)
+    # cv2.putText(frame, pred, (50,50) ,font, 1, (40, 40, 0), 2)
+
+
+    cv2.putText(frame, pred, org, font,  
+                    fontScale, color, thickness, cv2.LINE_AA) 
+
+
+    cv2.imshow('frame', frame)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+# def get_frame(video):
+#     vid = cv2.VideoCapture(video)
+#     _, fr = vd.read()
+
+#         _, jpeg = cv2.imencode('.jpg', fr)
+#         return jpeg.tobytes()
+
+
+
+'''
+    # returns camera frames along with bounding boxes and predictions
+    def get_frame(self):
+        _, fr = self.video.read()
+        #gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+        chord = chord_c.detectMultiScale(gray_fr, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            fc = gray_fr[y:y+h, x:x+w]#change gray_fr to color frame
+
+            roi = cv2.resize(fc, (48, 48))
+            pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+
+            cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+            cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+
+        _, jpeg = cv2.imencode('.jpg', fr)
+        return jpeg.tobytes()
+'''
+
+
+
+
+
+
+'''
+import cv2  # still used to save images out
+import os
+import numpy as np
+from decord import VideoReader
+from decord import cpu, gpu
+
+
+def extract_frames(video_path, frames_dir, overwrite=False, start=-1, end=-1, every=1):
+    """
+    Extract frames from a video using decord's VideoReader
+    :param video_path: path of the video
+    :param frames_dir: the directory to save the frames
+    :param overwrite: to overwrite frames that already exist?
+    :param start: start frame
+    :param end: end frame
+    :param every: frame spacing
+    :return: count of images saved
+    """
+
+    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
+    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
+
+    video_dir, video_filename = os.path.split(video_path)  # get the video path and filename from the path
+
+    assert os.path.exists(video_path)  # assert the video file exists
+
+    # load the VideoReader
+    vr = VideoReader(video_path, ctx=cpu(0))  # can set to cpu or gpu .. ctx=gpu(0)
+                     
+    if start < 0:  # if start isn't specified lets assume 0
+        start = 0
+    if end < 0:  # if end isn't specified assume the end of the video
+        end = len(vr)
+
+    frames_list = list(range(start, end, every))
+    saved_count = 0
+
+    if every > 25 and len(frames_list) < 1000:  # this is faster for every > 25 frames and can fit in memory
+        frames = vr.get_batch(frames_list).asnumpy()
+
+        for index, frame in zip(frames_list, frames):  # lets loop through the frames until the end
+            save_path = os.path.join(frames_dir, video_filename, "{:010d}.jpg".format(index))  # create the save path
+            if not os.path.exists(save_path) or overwrite:  # if it doesn't exist or we want to overwrite anyways
+                cv2.imwrite(save_path, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))  # save the extracted image
+                saved_count += 1  # increment our counter by one
+
+    else:  # this is faster for every <25 and consumes small memory
+        for index in range(start, end):  # lets loop through the frames until the end
+            frame = vr[index]  # read an image from the capture
+            
+            if index % every == 0:  # if this is a frame we want to write out based on the 'every' argument
+                save_path = os.path.join(frames_dir, video_filename, "{:010d}.jpg".format(index))  # create the save path
+                if not os.path.exists(save_path) or overwrite:  # if it doesn't exist or we want to overwrite anyways
+                    cv2.imwrite(save_path, cv2.cvtColor(frame.asnumpy(), cv2.COLOR_RGB2BGR))  # save the extracted image
+                    saved_count += 1  # increment our counter by one
+
+    return saved_count  # and return the count of the images we saved
+
+
+def video_to_frames(video_path, frames_dir, overwrite=False, every=1):
+    """
+    Extracts the frames from a video
+    :param video_path: path to the video
+    :param frames_dir: directory to save the frames
+    :param overwrite: overwrite frames if they exist?
+    :param every: extract every this many frames
+    :return: path to the directory where the frames were saved, or None if fails
+    """
+
+    video_path = os.path.normpath(video_path)  # make the paths OS (Windows) compatible
+    frames_dir = os.path.normpath(frames_dir)  # make the paths OS (Windows) compatible
+
+    video_dir, video_filename = os.path.split(video_path)  # get the video path and filename from the path
+
+    # make directory to save frames, its a sub dir in the frames_dir with the video name
+    os.makedirs(os.path.join(frames_dir, video_filename), exist_ok=True)
+    
+    print("Extracting frames from {}".format(video_filename))
+    
+    extract_frames(video_path, frames_dir, every=every)  # let's now extract the frames
+
+    return os.path.join(frames_dir, video_filename)  # when done return the directory containing the frames
+
+
+if __name__ == '__main__':
+    # test it
+    video_to_frames(video_path='test.mp4', frames_dir='test_frames', overwrite=False, every=5)
+'''
